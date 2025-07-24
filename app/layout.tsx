8ae3baa1f7fc53306cb9aa9af4c1b60b41c7bda2
SuporteTIMarciaCastro <suporteti@marciacastrosemijoias.com.br>
2025-07-24 11:12:23 -0300

    docs

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         </ThemeProvider>
         {/* Google Analytics */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-P75NCE9VG6"
+          src="https://www.googletagmanager.com/gtag/js?id=G-CWYLWTWDR4"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -42,7 +42,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-P75NCE9VG6', {
+            gtag('config', 'G-CWYLWTWDR4', {
               page_path: window.location.pathname,
             });
           `}
